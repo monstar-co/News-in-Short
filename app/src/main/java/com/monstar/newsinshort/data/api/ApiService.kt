5e2e7ca33434dc8bb5193dab0b4cf35415553c63
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-14 18:40:35 +0100

    Created ApiService

--- app/src/main/java/com/monstar/newsinshort/data/api/ApiService.kt
+++ app/src/main/java/com/monstar/newsinshort/data/api/ApiService.kt
@@ -1,4 +1,15 @@
 package com.monstar.newsinshort.data.api
 
+import com.monstar.newsinshort.data.entity.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("v2/top-headlines")
+    suspend fun getNewsHeadline(
+        @Query("country") country : String,
+        @Query("apiKey") apiKey : String = "eca9ac82ee2b4319919d3db7cddecc14",
+    ): Response<NewsResponse>
 }

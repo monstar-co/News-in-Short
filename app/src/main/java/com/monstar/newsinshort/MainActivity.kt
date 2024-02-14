796ee9fbb492787150e6ca1d4183401b21c12e69
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-14 10:02:13 +0100

    Added AppEntry to MainActivity

--- app/src/main/java/com/monstar/newsinshort/MainActivity.kt
+++ app/src/main/java/com/monstar/newsinshort/MainActivity.kt
@@ -3,23 +3,34 @@ package com.monstar.newsinshort
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.monstar.newsinshort.ui.navigation.AppNavigationGraph
 import com.monstar.newsinshort.ui.theme.NewsInShortTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewsInShortTheme {
-
+                Surface(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)) {
+                    AppEntryPoint()
+                }
             }
         }
     }
 }
 
+
+@Composable
+fun AppEntryPoint() {
+    AppNavigationGraph()
+}

7484be7f02767fe238b22991b6b4b3c83d2a0732
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-14 16:35:24 +0100

    Created a News viewModel

--- app/src/main/java/com/monstar/newsinshort/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/monstar/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -1,4 +1,24 @@
 package com.monstar.newsinshort.ui.viewmodel
 
-class NewsViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(): ViewModel() {
+
+    val value ="value"
+    init {
+        Log.d(TAG,"Init block of NewsViewmodel" )
+    }
+
+    fun printSomething() {
+        println("do something")
+    }
+
+    companion object {
+        const val TAG = "NewsViewmodel"
+    }
 }

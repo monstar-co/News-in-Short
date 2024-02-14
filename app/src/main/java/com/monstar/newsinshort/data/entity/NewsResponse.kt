e024f8dc9d4448473e66118b7f426b98b4484d58
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-14 18:46:59 +0100

    Added NewsResponse

--- app/src/main/java/com/monstar/newsinshort/data/entity/NewsResponse.kt
+++ app/src/main/java/com/monstar/newsinshort/data/entity/NewsResponse.kt
@@ -1,7 +1,7 @@
 package com.monstar.newsinshort.data.entity
 
 data class NewsResponse(
-    val articles: List<Article>,
     val status: String,
-    val totalResults: Int
+    val totalResults: Int,
+    val articles: List<Article>
 )

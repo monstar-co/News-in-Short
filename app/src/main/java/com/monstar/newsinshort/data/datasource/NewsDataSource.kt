b7f7fb85e6f3265d37fba51a6908e5c2941b04ad
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-14 18:45:29 +0100

    Created NewsDataSource

--- app/src/main/java/com/monstar/newsinshort/data/datasource/NewsDataSource.kt
+++ app/src/main/java/com/monstar/newsinshort/data/datasource/NewsDataSource.kt
@@ -1,4 +1,9 @@
 package com.monstar.newsinshort.data.datasource
 
+import com.monstar.newsinshort.data.entity.NewsResponse
+import retrofit2.Response
+import retrofit2.http.Query
+
 interface NewsDataSource {
+    fun getNewsHeadline(country : String): Response<NewsResponse>
 }

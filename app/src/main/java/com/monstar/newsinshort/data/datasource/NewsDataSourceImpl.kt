62db68ecca07fd1858cbce3b988677eecadd285b
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-15 18:26:36 +0100

    Created the news data source implementation and repository

--- app/src/main/java/com/monstar/newsinshort/data/datasource/NewsDataSourceImpl.kt
+++ app/src/main/java/com/monstar/newsinshort/data/datasource/NewsDataSourceImpl.kt
@@ -1,4 +1,15 @@
 package com.monstar.newsinshort.data.datasource
 
-class NewsDataSourceImpl {
+import com.monstar.newsinshort.data.api.ApiService
+import com.monstar.newsinshort.data.entity.NewsResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class NewsDataSourceImpl @Inject constructor(
+    private val apiService: ApiService
+) : NewsDataSource {
+
+    override suspend fun getNewsHeadline(country: String): Response<NewsResponse> {
+        return apiService.getNewsHeadline(country)
+    }
 }
--- app/src/main/java/com/monstar/newsinshort/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/monstar/newsinshort/ui/repository/NewsRepository.kt
@@ -1,4 +1,30 @@
 package com.monstar.newsinshort.ui.repository
 
-class NewsRepository {
+import com.monstar.newsinshort.data.datasource.NewsDataSource
+import com.monstar.newsinshort.data.entity.NewsResponse
+import com.monstar.utilities.ResourceState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class NewsRepository @Inject constructor(
+    private val newsDataSource: NewsDataSource
+) {
+
+    suspend fun getNewsHeadline(country: String): Flow<ResourceState<NewsResponse>> {
+        return flow {
+            emit(ResourceState.Loading())
+
+            val response = newsDataSource.getNewsHeadline(country)
+
+            if(response.isSuccessful && response.body() != null){
+                emit(ResourceState.Success(response.body()!!))
+            } else {
+                emit(ResourceState.Error("Error fetching news data"))
+            }
+        }.catch { e ->
+            emit(ResourceState.Error(e?.localizedMessage ?: "Some error in flow"))
+        }
+    }
 }

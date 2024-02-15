56379624ad1645b09f74f3c538525df651bdd973
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-15 18:26:57 +0100

    Created the News viewModel

--- app/src/main/java/com/monstar/newsinshort/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/monstar/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -2,21 +2,42 @@ package com.monstar.newsinshort.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.monstar.newsinshort.data.AppConstants.COUNTRY
+import com.monstar.newsinshort.data.entity.NewsResponse
+import com.monstar.newsinshort.ui.repository.NewsRepository
+import com.monstar.utilities.ResourceState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class NewsViewModel @Inject constructor(): ViewModel() {
+class NewsViewModel @Inject constructor(
+    private val newsRepository: NewsRepository
+): ViewModel() {
 
-    val value ="value"
+    private val _news: MutableStateFlow<ResourceState<NewsResponse>> = MutableStateFlow(ResourceState.Loading())
+    val news: StateFlow<ResourceState<NewsResponse>> = _news
+
     init {
-        Log.d(TAG,"Init block of NewsViewmodel" )
+        getNews(COUNTRY)
     }
 
-    fun printSomething() {
-        println("do something")
-    }
+ private fun getNews(country: String){
+     viewModelScope.launch(Dispatchers.IO) {
+         newsRepository.getNewsHeadline(country)
+             .collectLatest { newsResponse ->
+                _news.value = newsResponse
+             }
+     }
+ }
+
+
 
     companion object {
         const val TAG = "NewsViewmodel"

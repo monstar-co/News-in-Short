7d2179f658296365ca4597d8ec68fe5e31352301
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-15 18:27:49 +0100

    Implemented the ViewModel in the home screen

--- app/src/main/java/com/monstar/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/monstar/newsinshort/ui/screens/HomeScreen.kt
@@ -1,29 +1,54 @@
 package com.monstar.newsinshort.ui.screens
 
-import androidx.compose.foundation.background
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.monstar.newsinshort.ui.components.LoadingSpinner
+import com.monstar.newsinshort.ui.components.NewsList
 import com.monstar.newsinshort.ui.theme.NewsInShortTheme
 import com.monstar.newsinshort.ui.viewmodel.NewsViewModel
+import com.monstar.utilities.ResourceState
+
+const val TAG = "HomeScreen"
 
 @Composable
 fun HomeScreen(
-    newsViewModel: NewsViewModel = hiltViewModel()
-){
+    newsViewModel: NewsViewModel = hiltViewModel(),
+) {
+
+    val newsResponse by newsViewModel.news.collectAsState()
     Surface(
         modifier = Modifier.fillMaxSize()
-    ){
+    ) {
+        when (newsResponse) {
+            is ResourceState.Loading -> {
+                LoadingSpinner()
+                Log.d(TAG, "Inside Loading")
+            }
 
+            is ResourceState.Success -> {
+                val response = (newsResponse as ResourceState.Success).data
+                Log.d(TAG, "Inside Success ${response.status} ${response.totalResults}")
+                NewsList(response)
+            }
+
+            is ResourceState.Error -> {
+                val error = (newsResponse as ResourceState.Error)
+                Log.d(TAG, "Inside Error $error")
+            }
+        }
     }
 }
 
 @Preview
 @Composable
-fun HomeScreenPreview(){
+fun HomeScreenPreview() {
     NewsInShortTheme {
         HomeScreen()
     }

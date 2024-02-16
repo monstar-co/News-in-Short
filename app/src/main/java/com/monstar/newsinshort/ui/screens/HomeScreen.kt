f6d006f8be818d2d6fa12f120f39b281ccba5741
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-16 07:58:00 +0100

    Added viewpager to the home screen

--- app/src/main/java/com/monstar/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/monstar/newsinshort/ui/screens/HomeScreen.kt
@@ -1,31 +1,45 @@
 package com.monstar.newsinshort.ui.screens
 
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.monstar.newsinshort.ui.components.LoadingSpinner
 import com.monstar.newsinshort.ui.components.NewsList
+import com.monstar.newsinshort.ui.components.NewsRowComponent
 import com.monstar.newsinshort.ui.theme.NewsInShortTheme
 import com.monstar.newsinshort.ui.viewmodel.NewsViewModel
 import com.monstar.utilities.ResourceState
 
 const val TAG = "HomeScreen"
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     newsViewModel: NewsViewModel = hiltViewModel(),
 ) {
-
     val newsResponse by newsViewModel.news.collectAsState()
-    Surface(
-        modifier = Modifier.fillMaxSize()
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f
     ) {
+        100
+    }
+    VerticalPager(
+        state = pagerState,
+        modifier = Modifier.fillMaxSize(),
+        pageSize = PageSize.Fill,
+        pageSpacing = 8.dp
+    ) { page: Int ->
         when (newsResponse) {
             is ResourceState.Loading -> {
                 LoadingSpinner()
@@ -35,7 +49,10 @@ fun HomeScreen(
             is ResourceState.Success -> {
                 val response = (newsResponse as ResourceState.Success).data
                 Log.d(TAG, "Inside Success ${response.status} ${response.totalResults}")
-                NewsList(response)
+                if(response.articles.isNotEmpty()){
+
+                NewsRowComponent(page, response.articles[page])
+                }
             }
 
             is ResourceState.Error -> {
@@ -44,6 +61,10 @@ fun HomeScreen(
             }
         }
     }
+
+
+
+
 }
 
 @Preview
